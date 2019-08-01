d385451a3d905b15d46291dce5fb8ec5ca93364b
Matt Barson <matt.barson@gmail.com>
2019-08-01 19:18:04 +0100

    test

--- Plants.py
+++ Plants.py
@@ -2,13 +2,13 @@
 
 
 from plants import gui
-from plants import rhs_scrape
-import os
-from plants import resources_file
-
-
-if os.path.exists(f'{resources_file}/plants.pkl') is False:
-    rhs_scrape.main_downloader()
+# from plants import rhs_scrape
+# import os
+# from plants import resources_file
+#
+#
+# if os.path.exists(f'{resources_file}/plants.pkl') is False:
+#     rhs_scrape.main_downloader()
 
 
 app = gui.SelectionScreen()
