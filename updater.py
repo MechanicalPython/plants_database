0d13fb48b7bc191e4c5573f94734a9b02615a447
Temp User <tempuser@Macintosh.local>
2019-08-30 12:13:42 +0100

    updater now has --user flag and changes dir to plants dir

--- updater.py
+++ updater.py
@@ -5,9 +5,10 @@ import os
 
 
 def update():
+    os.chdir(f'{os.path.dirname(__file__)}')
     process = subprocess.Popen(["git", "pull", "origin", "master"], stdout=subprocess.PIPE)
     output = process.communicate()[0]
 
     req_file = f'{os.path.dirname(__file__)}/requirements.txt'
-    process = subprocess.Popen(["pip", "install", "-r", f'{req_file}'])
+    process = subprocess.Popen(["pip3", "install", "-r", f'{req_file}', "--user"])
     output = process.communicate()[0]
